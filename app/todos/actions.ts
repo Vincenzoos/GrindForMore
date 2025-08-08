3370cb5915990d78de70094bf8f8a954f00702a1
Viet Tran <vtra0041@student.monash.edu>
2025-08-08 12:57:43 +1000

    step5: update and delete todos

--- app/todos/actions.ts
+++ app/todos/actions.ts
@@ -1,5 +1,6 @@
 "use server"
 
+import { Todo } from "@/types/custom"
 import { createClient } from "@/utils/supabase/server"
 import { revalidatePath } from "next/cache"
 
@@ -27,6 +28,48 @@ export async function addTodo(formData:FormData) {
 
     if (error){
         throw new Error('Error adding todo task')
+    }
+
+    revalidatePath("/todos")
+}
+
+export async function deleteTodo(id:number) {
+    const supabase = await createClient()
+    const {data: {user}} = await supabase.auth.getUser()
+
+    if(!user){
+        throw new Error("Login required")
+    }
+
+    const {error} = await supabase.from("todos").delete().match({
+        user_id: user.id,
+        id: id,
+    })
+
+    if(error){
+        throw new Error("Error deleting task")
+    }
+
+    revalidatePath("/todos")
+}
+
+export async function updateTodo(todo:Todo) {
+    const supabase = await createClient()
+    const {data: {user}} = await supabase.auth.getUser()
+
+    if(!user){
+        throw new Error("Login required")
+    }
+
+    const {error} = await supabase.from("todos").update(todo).match(
+        {
+            user_id: user.id,
+            id: todo.id,
+        }
+    )
+
+    if(error){
+        throw new Error("Error updating task")
     }
 
     revalidatePath("/todos")
--- components/todo-item.tsx
+++ components/todo-item.tsx
@@ -1,8 +1,12 @@
+'use client'
+
+import { deleteTodo, updateTodo } from "@/app/todos/actions";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import { Checkbox } from "@/components/ui/checkbox";
 import { cn } from "@/lib/utils";
 import { Todo } from "@/types/custom";
+import { data } from "autoprefixer";
 import { Trash2 } from "lucide-react";
 
 export function TodoItem({ todo }: { todo: Todo }) {
@@ -18,10 +22,17 @@ export function TodoCard({ todo }: { todo: Todo }) {
     <Card className={cn("w-full")}>
       <CardContent className="flex items-start gap-3 p-3">
         <span className="size-10 flex items-center justify-center">
-          <Checkbox />
+          <Checkbox checked={Boolean(todo.is_complete)} onCheckedChange={async (val) =>{
+            // Prevent acidental updates when checkbox in ambiguous state
+            if (val === "indeterminate") return
+            // ...object: spread syntax for objects, uses to create a new object with all properties of the old object
+            await updateTodo({...todo, is_complete: val});
+          }} />
         </span>
         <p className={cn("flex-1 pt-2 min-w-0 break-words")}>{todo.task }</p>
-        <Button variant="ghost" size="icon">
+        <Button formAction={async (data) => {
+          await deleteTodo(todo.id)
+        }} variant="ghost" size="icon">
           <Trash2 className="h-5 w-5" />
           <span className="sr-only">Delete Todo</span>
         </Button>
